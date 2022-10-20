2bdf26a3406d2484697910d22b6af1f5314dd4de
noble6 <bitcoineazy@gmail.com>
2022-10-20 22:54:20 +0300

    CryptoUser & Asset models, Basic user creation ViewSet

--- backend/TrackerBackend/users/models.py
+++ backend/TrackerBackend/users/models.py
@@ -2,9 +2,35 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Asset(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название актива',
+        help_text='Введите название актива')
+    amount = models.DecimalField(
+        max_digits=50,
+        decimal_places=10,
+        verbose_name='Кол-во актива',
+        help_text='Введите кол-во актива')
+    add_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True
+    )
+    price = models.DecimalField(
+        max_digits=50,
+        decimal_places=10,
+        verbose_name="Цена закупки актива",
+        help_text="Введите цену покупки актива"
+    )
+
+
 class CryptoUser(AbstractUser):
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
     username = models.CharField(
         verbose_name='Имя пользователя', unique=True, max_length=100)
+    email = models.EmailField(
+        verbose_name='Адрес электронной почты', unique=True, max_length=150)
+    assets = models.ManyToManyField(Asset, verbose_name="Укажите активы и их количество")
 
     class Meta:
         verbose_name = 'Пользователь'
